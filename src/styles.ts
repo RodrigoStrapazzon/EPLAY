bcc245a9e21af9aaa212bb91d3b9a3422ddcdd41
RodrigoStrapazzon <Rodrigo93rs@gmail.com>
2024-10-24 18:43:08 -0300

    componentizando

--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const cores = {
   branca: '#EEEEEE',
@@ -20,10 +20,11 @@ export const GlobalCss = createGlobalStyle`
     background-color: ${cores.preta};
     color: ${cores.branca};
     padding-top: 40px;
+
+    .container {
+      max-width: 1024px;
+      width: 100%;
+      margin: 0 auto;
+    }
   }
-`
-export const Container = styled.div`
-  max-width: 1024px;
-  width: 100%;
-  margin: 0 auto;
 `
